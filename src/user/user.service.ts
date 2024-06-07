0a501b3f167ddf47d707a0131ea4284921860d08
Jonathan Costa <jonatasprogrammer@proton.me>
2024-06-07 11:19:01 -0300

    ORM treated

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,36 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PartialUserDto } from './dto/partial-user.dto';
 import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
   
+  constructor(private readonly prisma: PrismaService) {}
+
   async create(data: CreateUserDto) {
     data.password = await bcrypt.hash(data.password,await  bcrypt.genSalt());
-    return data;
-  }
+    return this.prisma.user.create({
+      data
+    })
+  };
 
   async findOne(id: number) {
-    return id;
-  }
+    return this.prisma.user.findFirst({
+      where:{
+        id
+      }
+    })
+  };
 
   async findAll() {
-    return [];
+    return this.prisma.user.findMany()
   }
 
   async update(id: number, data: UpdateUserDto) {
+    if (! await this.findOne(id)) {
+      throw new BadRequestException("Usuário não existe")
+    }
     data.password = await bcrypt.hash(data.password,await  bcrypt.genSalt());
-    return {id, data};
-  }
+    return this.prisma.user.update({
+      data,
+      where:{
+        id
+      }
+    })
+  };
 
   async partial(id: number, data: PartialUserDto) {
-    data.password = await bcrypt.hash(data.password,await  bcrypt.genSalt());
-    return {id, data};
-  }
+    if (! await this.findOne(id)) {
+      throw new BadRequestException("Usuário não existe")
+    }
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password,await  bcrypt.genSalt());  
+    }
+    return this.prisma.user.update({
+      data,
+      where:{
+        id
+      }
+    })
+  };
 
   async remove(id: number) {
-    return id;
+    if (! await this.findOne(id)) {
+      throw new BadRequestException("Usuário não encontrado")
+    }
+    return this.prisma.user.delete({
+      where:{id}
+    })
   }
-}
+};
