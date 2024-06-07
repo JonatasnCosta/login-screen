63080b87e341851209df10e4debb106c43491d5e
Jonathan Costa <jonatasprogrammer@proton.me>
2024-06-07 14:54:23 -0300

    Middleware treated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common'; 
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common'; 
 import { UserModule } from './user/user.module';
+import { CheckIdMiddleware } from './middlewares/check-id.middleware';
 
 @Module(
 {imports: [UserModule], 
@@ -7,4 +8,11 @@ controllers: [],
 providers: [], 
 }) 
 
-export class AppModule {}
+export class AppModule implements NestModule{
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(CheckIdMiddleware).forRoutes({
+            path:"users/:id",
+            method: RequestMethod.ALL
+        })
+    }
+}
